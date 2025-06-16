e7191602170a891054b97db106cef070ee00a6e0
mminsong <pamten@naver.com>
2025-06-16 11:42:32 +0900

    로그인 창

---
+++ src/assets/App.tsx
@@ -0,0 +1,17 @@
+import { Header } from "./common";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="min-h-screen"
+      style={{ backgroundColor: "var(--color-background)" }}
+    >
+      <Header title="Sellog" />
+
+      {/* 헤더가 fixed이므로 padding-top 추가 */}
+    </div>
+  );
+}
+
+export default App;
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,8 +1,30 @@
 import React from "react";
+const findStyles = {
+  color: "var(--color-text-secondary)",
+};
 const Login = () => {
   return (
-    <div>
-      <div className="p-10">로그인</div>
+    <div className="fixed w-[400px] h-[300px] top-2/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div
+        className="p-10 text-7xl font-bold"
+        style={{ color: "var(--color-primary)" }}
+      >
+        SelLog
+      </div>
+      <div className="card">
+        <input className="input flex my-2" placeholder="아이디" />
+        <input className="input flex my-2" placeholder="비밀번호" />
+        <button className="btn-login btn">회원가입</button>
+        <button className="btn-login btn">로그인</button>
+        <div className="flex w-full ">
+          <div className="flex w-5/6 text-sm" style={findStyles}>
+            비밀번호 찾기
+          </div>
+          <div className="flex justify-end w-full text-sm" style={findStyles}>
+            아이디 찾기
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
